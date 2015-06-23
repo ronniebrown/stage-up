35933e3f8a5f37c1c76fd76c772a0c5c16acdb3e
Simon <sburns77@gmail.com>
2015-06-23 17:04:08 -0500

    Added questionnair files and started building out form structure.

--- public/modules/articles/config/articles.client.routes.js
+++ public/modules/articles/config/articles.client.routes.js
@@ -21,6 +21,14 @@ angular.module('articles').config(['$stateProvider',
 			url: '/quest1',
 			templateUrl: 'modules/articles/views/quest1.html'
 		}).
+		state('quest2', {
+			url: '/quest2',
+			templateUrl: 'modules/articles/views/quest2.html'
+		}).
+		state('quest3', {
+			url: '/quest3',
+			templateUrl: 'modules/articles/views/quest3.html'
+		}).
 		state('editArticle', {
 			url: '/articles/:articleId/edit',
 			templateUrl: 'modules/articles/views/edit-article.client.view.html'
