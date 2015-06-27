5530450be482162731ac3cd7602258381609fc88
jonathan seneris <jonathanpseneris@gmail.com>
2015-06-27 02:27:21 -0700

    basic routing sketch

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -7,16 +7,44 @@ var users = require('../../app/controllers/users.server.controller'),
 	articles = require('../../app/controllers/articles.server.controller');
 
 module.exports = function(app) {
-	// Article Routes
-	app.route('/articles')
-		.get(articles.list)
-		.post(users.requiresLogin, articles.create);
+		// Article Routes
+		app.route('/articles')
 
-	app.route('/articles/:articleId')
-		.get(articles.read)
-		.put(users.requiresLogin, articles.hasAuthorization, articles.update)
-		.delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
 
-	// Finish by binding the article middleware
-	app.param('articleId', articles.articleByID);
-};
+
+		.get(function(req, res, next) {
+					mongoose.model('User').find({}, function(err, users) {
+						if (err) {
+							return console.error(err);
+						} else {
+							// send back all the profiles
+						}
+					})
+				}
+
+
+
+				.post(function(formData) { // this should be the form data. how to link user and profile without foreign keys?
+						mongoose.model('User').create(formData);
+					}, function(err, user) {
+						if (err) {
+							res.send("Problem");
+						} else {
+							console.log("user created: ", formData.basic.username)
+							res.format({
+								res.location("profile");
+								res.redirect("/profile");
+							})
+						}
+					};
+
+
+
+					app.route('/articles/:articleId')
+					.get(articles.read)
+					.put(users.requiresLogin, articles.hasAuthorization, articles.update)
+					.delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
+
+					// Finish by binding the article middleware
+					app.param('articleId', articles.articleByID);
+				};
